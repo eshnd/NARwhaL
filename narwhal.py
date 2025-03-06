d32f3d6ec6fc9f7233bf69aaf7dd0145b4eec0c7
eshaan <eshdesh@null.net>
2025-03-05 18:10:38 -0600

    added normalizing

--- narwhal.py
+++ narwhal.py
@@ -9,8 +9,19 @@ datapoints = [ # features, output, reward
 
 input_features = [8, 9, 14] # features
 
-def narwhal(input_features, datapoints): 
+def narwhal(input_features, datapoints_old): 
+    datapoints = datapoints_old
+    combined = []
+    for th in datapoints:
+        combined += th
+    combined += input_features
+    maxc = max(th)
+    minc = min(th)
 
+    for j in range(len(datapoints)):
+        for i in range(len(datapoints[j])):
+            datapoints[j][i] = (datapoints[j][i] - minc) / (maxc - minc)
+    
     closest_points = []
 
     for point in datapoints:
